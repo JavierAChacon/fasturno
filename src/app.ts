282bc820ac320c0fa881778789a09e44d22df82d
Javier <javierchaconram@gmail.com>
2025-09-09 13:04:35 -0400

    chore: add initial database schema and authentication setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 import type { Express } from 'express'
+import { toNodeHandler } from 'better-auth/node'
+import { auth } from './auth'
 
 const app: Express = express()
+
+app.all('/api/auth/*splat', toNodeHandler(auth))
 
 app.use(express.json())
 
--- src/tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-import request from 'supertest'
-import app from '../app'
-
-it('should return status 200', async () => {
-  const response = await request(app).get('/')
-  expect(response.status).toBe(200)
-  expect(response.body.message).toBe('Hello World!')
-})
