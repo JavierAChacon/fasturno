d3915053935d842f715d5759b27686b2125d6f47
Javier <javierchaconram@gmail.com>
2025-09-27 14:15:37 -0400

    fix(layout): replace router.push with useEffect for session handling

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -3,14 +3,17 @@ import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import AppSidebar from './components/AppSidebar'
 import { useRouter } from 'next/navigation'
 import { useGetSession } from '@/queries/auth'
+import { useEffect } from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { data: session, isPending } = useGetSession()
   const router = useRouter()
 
-  if (!session && !isPending) {
-    router.push('/sign-in')
-  }
+  useEffect(() => {
+    if (!session && !isPending) {
+      router.replace('/sign-in')
+    }
+  }, [session, isPending, router])
 
   return (
     <SidebarProvider>
